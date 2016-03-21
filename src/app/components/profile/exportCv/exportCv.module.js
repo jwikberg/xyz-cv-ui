ac72fa6045bde01f2a72f2ca4da6104b8e7a1d73
Joel Wikberg <joel.wikberg@gmail.com>
2016-03-21 10:47:26 +0100

    test 4

--- src/app/components/profile/exportCv/exportCv.module.js
+++ src/app/components/profile/exportCv/exportCv.module.js
@@ -1,12 +1,6 @@
 (function() {
     'use strict';
 
-    angular.module('xyz-cv-ui.profile.exportCv', [
-        'shared.skills',
-        'shared.languages',
-        'shared.others',
-        'shared.courses',
-        'shared.offices'
-    ]);
+    angular.module('xyz-cv-ui.profile.exportCv', []);
 
 })();
